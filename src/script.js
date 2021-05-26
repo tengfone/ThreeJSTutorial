907c845d6a8851e99760e5ab3cc7f78e91a90410
tengfone <phangtf@gmail.com>
2021-05-26 20:47:14 +0800

    Updates

--- src/script.js
+++ src/script.js
@@ -8,53 +8,8 @@ const scene = new THREE.Scene()
 const geometry = new THREE.BoxGeometry(1, 1, 1)
 const material = new THREE.MeshBasicMaterial({ color: 'red' })
 const mesh = new THREE.Mesh(geometry, material)
-// mesh.position.x = 0.7
-// mesh.position.y = -0.6
-// mesh.position.z = 1
-mesh.position.set(0.7, -0.6, 1)
 
-console.log(mesh.position.length()) // Length of vector Distance of center of scene to object position
-// mesh.position.normalize() // Take vector length reduce to 1
-
-// Scale
-mesh.scale.set(2, 0.5, 0.5)
-
-// Rotation Pi = Half rotation. Full rotation = 2pi
-mesh.rotation.reorder('YXZ')
-mesh.rotation.x = Math.PI / 4
-mesh.rotation.y = Math.PI / 4
-
-
-// scene.add(mesh)
-
-// Objects
-const group = new THREE.Group()
-group.position.y = 1
-scene.add(group)
-
-const cube1 = new THREE.Mesh(
-    new THREE.BoxGeometry(1, 1, 1),
-    new THREE.MeshBasicMaterial({ color: 'White' })
-)
-group.add(cube1)
-
-const cube2 = new THREE.Mesh(
-    new THREE.BoxGeometry(1, 1, 1),
-    new THREE.MeshBasicMaterial({ color: 'Blue' })
-)
-cube2.position.x = -2
-group.add(cube2)
-
-const cube3 = new THREE.Mesh(
-    new THREE.BoxGeometry(1, 1, 1),
-    new THREE.MeshBasicMaterial({ color: 'Red' })
-)
-cube3.position.x = 2
-group.add(cube3)
-
-// Axes Helper
-const axesHelper = new THREE.AxesHelper()
-scene.add(axesHelper)
+scene.add(mesh)
 
 // Sizes
 const sizes = {
@@ -77,4 +32,22 @@ const renderer = new THREE.WebGLRenderer({
 })
 renderer.setSize(sizes.width, sizes.height)
 
-renderer.render(scene, camera)
+// Clock
+const clock = new THREE.Clock()
+
+// Animation
+const tick = () => {
+    // Clock
+    const elapsedTime = clock.getElapsedTime()
+
+    // Update Objects
+    // mesh.rotation.y = elapsedTime * Math.PI * 2 // 1 Rev per sec
+    mesh.position.y = Math.sin(elapsedTime)
+
+    // Render
+    renderer.render(scene, camera)
+
+    window.requestAnimationFrame(tick)
+}
+
+tick()
