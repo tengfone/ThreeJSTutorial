466aaf6b6da12a9955321e20e19f3047f861c6d1
tengfone <phangtf@gmail.com>
2021-05-27 23:19:29 +0800

    nxt L9

--- src/script.js
+++ src/script.js
@@ -1,87 +1,23 @@
 import './style.css'
 import * as THREE from 'three'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
-import * as dat from 'dat.gui'
-import typefaceFont from 'three/examples/fonts/helvetiker_regular.typeface.json'
 
 /**
  * Base
  */
-// Debug
-const gui = new dat.GUI()
-
 // Canvas
 const canvas = document.querySelector('canvas.webgl')
 
 // Scene
 const scene = new THREE.Scene()
 
-/**
- * Textures
- */
-const textureLoader = new THREE.TextureLoader()
-const matcapTexture = textureLoader.load('textures/matcaps/8.png')
+// Object
+const geometry = new THREE.BoxGeometry(1, 1, 1)
+const material = new THREE.MeshBasicMaterial({ color: 0xff0000 })
+const mesh = new THREE.Mesh(geometry, material)
+scene.add(mesh)
 
-/**
- * Fonts
- */
-const fontLoader = new THREE.FontLoader()
-
-fontLoader.load(
-    '/fonts/helvetiker_regular.typeface.json',
-    (font) =>
-    {
-        fontLoader.load(
-            '/fonts/helvetiker_regular.typeface.json',
-            (font) =>
-            {
-                // Material
-                const material = new THREE.MeshMatcapMaterial({ matcap: matcapTexture })
-
-                // Text
-                const textGeometry = new THREE.TextBufferGeometry(
-                    'Teng Fone Experimental ThreeJS',
-                    {
-                        font: font,
-                        size: 0.5,
-                        height: 0.2,
-                        curveSegments: 12,
-                        bevelEnabled: true,
-                        bevelThickness: 0.03,
-                        bevelSize: 0.02,
-                        bevelOffset: 0,
-                        bevelSegments: 5
-                    }
-                )
-                textGeometry.center()
-
-                const text = new THREE.Mesh(textGeometry, material)
-                scene.add(text)
-
-                // Donuts
-                const donutGeometry = new THREE.TorusBufferGeometry(0.3, 0.2, 32, 64)
-
-                for(let i = 0; i < 100; i++)
-                {
-                    const donut = new THREE.Mesh(donutGeometry, material)
-                    donut.position.x = (Math.random() - 0.5) * 10
-                    donut.position.y = (Math.random() - 0.5) * 10
-                    donut.position.z = (Math.random() - 0.5) * 10
-                    donut.rotation.x = Math.random() * Math.PI
-                    donut.rotation.y = Math.random() * Math.PI
-                    const scale = Math.random()
-                    donut.scale.set(scale, scale, scale)
-
-                    scene.add(donut)
-                }
-            }
-        )
-    }
-)
-
-/**
- * Sizes
- */
+// Sizes
 const sizes = {
     width: window.innerWidth,
     height: window.innerHeight
@@ -102,32 +38,23 @@ window.addEventListener('resize', () =>
     renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
 })
 
-/**
- * Camera
- */
-// Base camera
+// Camera
 const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 100)
-camera.position.x = 1
-camera.position.y = 1
-camera.position.z = 2
+camera.position.z = 3
 scene.add(camera)
 
 // Controls
 const controls = new OrbitControls(camera, canvas)
 controls.enableDamping = true
 
-/**
- * Renderer
- */
+// Renderer
 const renderer = new THREE.WebGLRenderer({
     canvas: canvas
 })
 renderer.setSize(sizes.width, sizes.height)
 renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
 
-/**
- * Animate
- */
+// Animate
 const clock = new THREE.Clock()
 
 const tick = () =>
